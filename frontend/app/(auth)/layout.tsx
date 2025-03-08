be26bb6fbb995eec0e2f67f375b7aff390271eb7
Shahmeer Ahmed <ahmedshahmeer486@gmail.com>
2025-03-08 23:24:57 +0500

    fix-login

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import "..(auth)/global.css"
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Sidebar } from "@/components/sidebar"
